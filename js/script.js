522ffc125b300b3e2ea23588855c3dca25991f35
AnonDEV23 <luisayvalballesteros@icloud.com>
2023-06-28 08:56:13 -0500

    So Many Changes lol

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const BURGER = document.querySelector(".burger");
+const NAV = document.querySelector(".mainUl");
+
+BURGER.addEventListener("click", () =>{
+    BURGER.classList.toggle("active");
+    NAV.classList.toggle("active");
+})
+
